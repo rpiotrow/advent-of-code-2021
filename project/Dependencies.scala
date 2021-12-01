926c77e809be7191a46ffa5df38c9a10dead7ead
Rafal Piotrowski <rpiotrowski@iteratorshq.com>
2021-12-02 00:17:22 +0100

    Add parsing command line options `-d` and `--day`

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,11 +2,13 @@ import sbt._
 
 object Versions {
   val zio       = "2.0.0-M6-2"
+  val scopt     = "4.0.1"
 }
 
 object Dependencies {
   lazy val zio        = "dev.zio"          %% "zio"               % Versions.zio
   lazy val zioStreams = "dev.zio"          %% "zio-streams"       % Versions.zio
+  lazy val scopt      = "com.github.scopt" %% "scopt"             % Versions.scopt
 }
 
 object TestDependencies {
--- src/main/scala/io/github/rpiotrow/advent2021/Main.scala
+++ src/main/scala/io/github/rpiotrow/advent2021/Main.scala
@@ -1,5 +1,6 @@
 package io.github.rpiotrow.advent2021
 
+import scopt.OParser
 import io.github.rpiotrow.advent2021.day01.SonarSweep
 import zio.*
 
@@ -9,12 +10,24 @@ private val days: Map[Int, Solution] = Map(
   1 -> SonarSweep.solution
 )
 
-private def solution(day: Int) =
-  for
-    _ <- Console.printLine(s"Day: $day")
-    _ <- days.getOrElse(day, ZIO.fail("There is no such day!!!"))
-  yield ()
-
 object Main extends ZIOAppDefault:
+
+  private def parseArgs(args: Seq[String]) =
+    OParser.parse(CliConfig.parser, args, CliConfig()) match
+      case Some(config) =>
+        ZIO.succeed(config)
+      case _ =>
+        ZIO.fail("Invalid parameters!!!")
+
+  private def solution(day: Int) =
+    for
+      _ <- Console.printLine(s"Day: $day")
+      _ <- days.getOrElse(day, ZIO.fail("There is no such day!!!"))
+    yield ()
+
   def run =
-    ZIO.collectAll(days.keys.map(solution))
+    for
+      args <- getArgs
+      cli <- parseArgs(args)
+      result <- cli.day.map(solution).getOrElse(ZIO.foreach(days.keys)(solution))
+    yield result
