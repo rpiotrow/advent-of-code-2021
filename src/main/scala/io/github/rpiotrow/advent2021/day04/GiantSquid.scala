86be0a2a6a3fa6ce493cab42cb6bd557c8d0446f
Rafal Piotrowski <rpiotrowski@iteratorshq.com>
2021-12-06 20:50:01 +0100

    fix: write results on console in day 04

--- src/main/scala/io/github/rpiotrow/advent2021/day04/GiantSquid.scala
+++ src/main/scala/io/github/rpiotrow/advent2021/day04/GiantSquid.scala
@@ -1,6 +1,6 @@
 package io.github.rpiotrow.advent2021.day04
 
-import zio.{Console, ZIO}
+import zio.{Console, ZEnv, ZIO}
 import zio.stream.{ZSink, ZStream}
 import io.github.rpiotrow.advent2021.Solution
 
@@ -14,22 +14,27 @@ object GiantSquid:
         .peel(ZSink.fold(Option.empty[Int] -> setup.startState) { case (_, game) =>
           game.ranking.isEmpty
         }(foldGame))
-        .use { case ((firstWinningBoardNumberOption, firstWinningBoardState), restOfStream) =>
-          for
-            firstWinningBoardNumber <- ZIO.getOrFailWith("first winning board number does not exist")(
-              firstWinningBoardNumberOption
-            )
-            firstWinningBoard <- ZIO.getOrFailWith("first winning board not exists")(firstWinningBoardState.ranking.headOption)
-            firstWinningBoardScore = score(firstWinningBoardNumber, firstWinningBoard)
-            lastNumberAndState <- restOfStream
-              .runFoldWhile(firstWinningBoardNumberOption -> firstWinningBoardState) { case (_, game) =>
-                game.ranking.size < game.boards.size
-              }(foldGame)
-            (lastWinningBoardNumberOption, lastWinningBoardState) = lastNumberAndState
-            lastWinningBoardNumber <- ZIO.getOrFailWith("last winning board number does not exist")(lastWinningBoardNumberOption)
-            lastWinningBoard <- ZIO.getOrFailWith("last winning board not exists")(lastWinningBoardState.ranking.lastOption)
-            lastWinningBoardScore = score(lastWinningBoardNumber, lastWinningBoard)
-          yield (firstWinningBoardScore.toLong, lastWinningBoardScore.toLong)
+        .use[ZEnv, String | java.io.IOException, (Long, Long)] {
+          case ((firstWinningBoardNumberOption, firstWinningBoardState), restOfStream) =>
+            for
+              firstWinningBoardNumber <- ZIO.getOrFailWith("first winning board number does not exist")(
+                firstWinningBoardNumberOption
+              )
+              firstWinningBoard <- ZIO.getOrFailWith("first winning board not exists")(firstWinningBoardState.ranking.headOption)
+              firstWinningBoardScore = score(firstWinningBoardNumber, firstWinningBoard)
+              _ <- Console.printLine(s"Score of first winning board is $firstWinningBoardScore")
+              lastNumberAndState <- restOfStream
+                .runFoldWhile(firstWinningBoardNumberOption -> firstWinningBoardState) { case (_, game) =>
+                  game.ranking.size < game.boards.size
+                }(foldGame)
+              (lastWinningBoardNumberOption, lastWinningBoardState) = lastNumberAndState
+              lastWinningBoardNumber <- ZIO.getOrFailWith("last winning board number does not exist")(
+                lastWinningBoardNumberOption
+              )
+              lastWinningBoard <- ZIO.getOrFailWith("last winning board not exists")(lastWinningBoardState.ranking.lastOption)
+              lastWinningBoardScore = score(lastWinningBoardNumber, lastWinningBoard)
+              _ <- Console.printLine(s"Score of last winning board is $lastWinningBoardScore")
+            yield (firstWinningBoardScore.toLong, lastWinningBoardScore.toLong)
         }
     yield solution
 
